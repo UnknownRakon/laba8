d2ef6f6a9176e035a14cee074736e0f5c82679bb
Vlad <gorenfeo@gmail.com>
2020-12-14 21:48:06 +0300

    two

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ for (let i = 1; i <= ovzi; i++){
 }
 // Задание 2
 console.log('Задание 2')
+for (let i = 0; i <= 15; i++){
+    if (i % 2 == 0) console.log(i + ' чётное')
+    else console.log(i + ' нечётное')
+}
