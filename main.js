200a4783d26f8e4672f137679ee32419b6b39f61
Vlad <gorenfeo@gmail.com>
2020-12-14 21:59:05 +0300

    three

--- main.js
+++ main.js
@@ -10,3 +10,9 @@ for (let i = 0; i <= 15; i++){
     if (i % 2 == 0) console.log(i + ' чётное')
     else console.log(i + ' нечётное')
 }
+// Задание 3
+console.log('Задание 3')
+for (let i = 0; i <= 8; i++) {
+    if (i % 2 == 0) console.log(' # # # #')
+    else console.log('# # # # ')
+}
