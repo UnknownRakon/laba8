41c57e0285ee4c03a0ed20253dba8d7b6b91fb80
Vlad <gorenfeo@gmail.com>
2020-12-14 21:35:14 +0300

    one

---
+++ main.js
@@ -0,0 +1,8 @@
+// Задание 1
+console.log('Задание 1')
+let ovzi = prompt('Введите количество овец', [0]);
+for (let i = 1; i <= ovzi; i++){
+    console.log(i + ' овечка')
+}
+// Задание 2
+console.log('Задание 2')
